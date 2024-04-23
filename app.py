2e36dd658dc9e49624b888ab4b83c77c29c7e4aa
P3naderia <136984986+P3naderia@users.noreply.github.com>
2024-04-23 14:47:15 +0900

    Update app.py

--- app.py
+++ app.py
@@ -20,20 +20,26 @@ GA_SCRIPT = """
 """
 
 def inject_ga(): 
-    
     index_path = pathlib.Path(st.__file__).parent / "static" / "index.html"
     soup = BeautifulSoup(index_path.read_text(), features="html.parser")
     if not soup.find(id=GA_ID): 
         bck_index = index_path.with_suffix('.bck')
-        if bck_index.exists():
-            shutil.copy(bck_index, index_path)  
-        else:
-            shutil.copy(index_path, bck_index)  
+        try:
+            if bck_index.exists():
+                shutil.copy(bck_index, index_path)  
+            else:
+                shutil.copy(index_path, bck_index)  
+        except Exception as e:
+            st.error(f"Error occurred while copying file: {e}")
+            return
         html = str(soup)
         new_html = html.replace('<head>', '<head>\n' + GA_SCRIPT)
-        index_path.write_text(new_html)
-
-inject_ga()
+        try:
+            index_path.write_text(new_html)
+        except Exception as e:
+            st.error(f"Error occurred while writing HTML: {e}")
+            # 복사된 파일을 다시 원래대로 되돌리기
+            shutil.copy(bck_index, index_path)
 
 
 def get_stats_valuation(ticker, headers = {'User-agent': 'Mozilla/5.0'}):
